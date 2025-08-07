f242d32ba2125fd57bc28ee659afde8ed2387467
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-08 00:23:21 +0530

    25223

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.conf import settings
 
 def homepage(request):
-    restaurant_name = settings.RESTAURANT_NAME
-    restaurant_phone = settings.RESTAURANT_PHONE
-    return render(request, 'home/homepage.html', {'restaurant_name': restaurant_name,'restaurant_phone': restaurant_phone})
+    restaurant = RestaurantInfo.objects.first()
+    return render(request, 'home/homepage.html', {
+        'restaurant_name': restaurant_name,
+        'restaurant_phone': restaurant_phone
+        })
