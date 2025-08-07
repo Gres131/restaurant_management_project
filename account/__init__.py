976d273c37c87218cc98786bdd5f059903159575
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-07 09:09:55 +0530

    24474

---
+++ account/__init__.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets
+from rest_framework.permission import IsAuthenticated
+from rest_framework.authtoken.models import Token 
+from rest_framework.authtoken.views import obtain_auth_token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Mete:
+        model = Note
+        fields = '__all__'
+        read_only_fields = ['owner']
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    permission_classes =[Is IsAuthenticated]
+
+    def get_queryset(self):
+        return Note.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serialize.save(owner=self.request.user)
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        token.objects.create(user=instance)
+    
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSSES':[
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+}
