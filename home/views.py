6f7468e8665bd07c97b9a243d7d2000385de1a9a
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-08 00:58:41 +0530

    25223

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.conf import RestaurantInfo
 
 def homepage(request):
     restaurant = RestaurantInfo.objects.first()
+
+    restaurant_name = restaurant.name if restaurant else settings.RESTAURANT_NAME
+    restaurant_phone = restaurant.phone if restaurant else settings.RESTAURANT_PHONE
+
+    
     return render(request, 'home/homepage.html', {
         'restaurant_name': restaurant_name,
         'restaurant_phone': restaurant_phone
