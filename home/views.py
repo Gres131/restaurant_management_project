56be82789fd5ea24e5d9f8a0fc112cc55c00afcb
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-07 19:31:25 +0530

    24475

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.conf import settings
 def homepage(request):
     restaurant_name = settings.RESTAURANT_NAME
     return render(request, 'home/homepage.html', {'restaurant_name': restaurant_name})
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
