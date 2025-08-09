0817405ebdd9afed6b95d7dcf96bdb419658166b
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-09 16:13:57 +0530

    25222

--- home/views.py
+++ home/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.conf import RestaurantInfo
 
 def homepage(request):
-    restaurant = RestaurantInfo.objects.first()
+    restaurant_name = settings.RESTAURANT_NAME
+    return render(request, 'home/homepage.html', {'restaurant_name': restaurant_name})
 
-    restaurant_name = restaurant.name if restaurant else settings.RESTAURANT_NAME
-    restaurant_phone = restaurant.phone if restaurant else settings.RESTAURANT_PHONE
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
 
-    
-    return render(request, 'home/homepage.html', {
-        'restaurant_name': restaurant_name,
-        'restaurant_phone': restaurant_phone
-        })
+def contact_us(request):
+    return render(request, 'home/contact_us.html')
