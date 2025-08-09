cab24d5aece6ea6aada85afd9d65bd22a36a78ff
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-09 17:03:57 +0530

    25224

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 from home.views import homepage
 from django.conf.urls import handler404
+from . import views
 
 handler404 = 'home.views.custom_404_view'
 
-urlpatterns = [ path('', homepage, name='homepage'), path('admin/', admin.site.urls), path('menu/', views.menu_list, name='menu_list'), path('contact/', views.contact_us, name='contact_us')]
+urlpatterns = [ path('', homepage, name='homepage'), path('admin/', admin.site.urls), path('menu/', views.menu_list, name='menu_list'), path('contact/', views.contact_us, name='contact_us'), path("", views.homepage, name = "homepage")]
