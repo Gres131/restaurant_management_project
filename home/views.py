4e4d9ccfbb1a0acdafd5ea17b6f521278b756ead
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-10 22:01:16 +0530

    26982

--- home/views.py
+++ home/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 def homepage(request):
-    context = {
-    "restaurant_name": settings.RESTAURANT_NAME
-    }
-    return render(request, 'home/homepage.html', context)
+    try:
+        restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'Our Restaurant')
+
+
+        context = {
+            "restaurant_name": settings.RESTAURANT_NAME
+        }
+        return render(request, 'home/homepage.html', context)
 
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)
