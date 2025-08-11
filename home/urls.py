668551c0f6ba6ba2815bc0f6630685f2e9938024
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-11 13:39:49 +0530

    26978

--- home/urls.py
+++ home/urls.py
@@ -6,11 +6,4 @@ from . import views
 
 handler404 = 'home.views.custom_404_view'
 
-urlpatterns = [ 
-    path('', homepage, name='homepage'), 
-    path('admin/', admin.site.urls), 
-    path('menu/', views.menu_list, name='menu_list'),
-     path('contact/', views.contact_us, name='contact_us'), 
-     path("", views.homepage, name = "homepage") 
-     path('reservation/', views.reservations, name='reservations'),
-     ]
+urlpatterns = [ path('', homepage, name='homepage'), path('admin/', admin.site.urls), path('menu/', views.menu_list, name='menu_list'), path('contact/', views.contact_us, name='contact_us'), path("", views.homepage, name = "homepage"), path('reservations/' views.reservations, name='reservations')]
