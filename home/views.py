29023b826a5ef45517835e4dfdac24a88514e315
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-11 16:16:05 +0530

    28779

--- home/views.py
+++ home/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.conf import settings
+from .models  import RestaurantInfo
+import logging 
+
+logger = logging.getLogger(__name__)
 
 def homepage(request):
-    context = {
-    "restaurant_name": settings.RESTAURANT_NAME
-    }
-    return render(request, 'home/homepage.html', context)
+    try:
+        restaurant = RestaurantInfo.objects.first()
+    except Exception as e:
+        logger.error(f"Database error fetching restaurant info: {e}")
+        restaurant = None
 
-def custom_404_view(request, exception):
-    return render(request, '404.html', status=404)
 
-def contact_us(request):
-    return render(request, 'home/contact_us.html')
+    restaurant_name = restaurant.name if restaurant else settings.RESTAURANT_NAME
+    restaurant_phone = restaurant.phone if restaurant else settings.RESTAURANT_PHONE
 
-def reservations(request):
-    return render(request, 'home/reservations.html')
+    
+    return render(request, 'home/homepage.html', {
+        'restaurant_name': restaurant_name,
+        'restaurant_phone': restaurant_phone
+        })
