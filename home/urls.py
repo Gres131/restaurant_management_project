0081570b01e0ccdbece961d31ec10500b96ec44d
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-11 19:09:47 +0530

    28778

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,14 @@ from .views import *
 from home.views import homepage
 from django.conf.urls import handler404
 from . import views
+from .views import feedback_view
 
 handler404 = 'home.views.custom_404_view'
 
-urlpatterns = [ path('', homepage, name='homepage'), path('admin/', admin.site.urls), path('menu/', views.menu_list, name='menu_list'), path('contact/', views.contact_us, name='contact_us'), path("", views.homepage, name = "homepage"), path('reservations/' views.reservations, name='reservations'),]
+urlpatterns = [ path('', homepage, name='homepage'), 
+path('admin/', admin.site.urls), 
+path('menu/', views.menu_list, name='menu_list'), 
+path('contact/', views.contact_us, name='contact_us'), 
+path("", views.homepage, name = "homepage"), 
+path('reservations/' views.reservations, name='reservations'),
+path('feedback/', views.feedback, name='feedback'),]
