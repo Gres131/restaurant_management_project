c7b425885abad9e713b7805442d4a8d47785a3bd
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-12 21:57:06 +0530

    29225

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,17 @@ from .models import *
 
 
 # Custom Admins
+class MenuAdmin(admin.ModelAdmin):
+   list_display = ('name', 'price','category', 'available')
+   list_filter = ('category', 'available')
+   search_fields = ('name', 'description')
+   ordering = ('name',)
+
+
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
+    list_display = ('item_name','item_price', 'created_at')
 
 
 # Register your models here.
+admin.site.register(Menu, MenuAdmin)
 admin.site.register(Item,ItemAdmin)
