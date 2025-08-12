72d3b8cd4ff861283b85f7ee8dafb4df231feee7
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-12 15:52:52 +0530

    29222

--- home/models.py
+++ home/models.py
@@ -12,7 +12,11 @@ class RestaurantInfo(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
     phone_number = models.CharField(max_length=15, blank=True)
+    
+    
 
 
     def __str__(self):
