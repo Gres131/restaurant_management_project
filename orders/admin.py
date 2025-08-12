0459e250e70663c1ade775ccce8ce5059a34dd02
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-12 23:11:08 +0530

    29225

--- orders/admin.py
+++ orders/admin.py
@@ -1,12 +1,15 @@
-from django.contrib import admin
+from django.contrib import admin #  Added missing import
 from .models import Order
 
 # Register your models here.
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id','customer','status', 'total_amount', 'created_at')
     list_filter = ('status','created_at')
-    search_fields = ('customer_username', 'customer_email')
+    search_fields = ('customer_username', 'customer_email')#Corrected filed names
     ordering = ('-created_at',)
+
+
+    #Only keep if `order_item` is actually a ManyToManyField in Order model
     filter_horizontal = ('order_items',)
 
 
