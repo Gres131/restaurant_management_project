d44875dde64dd3fbcc3cf33a78cdbf072a070703
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-14 14:41:06 +0530

    29226

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -15,19 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from home.view import homepage, menu_list
+from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
     path('admin/', admin.site.urls),
-    path('', homepage, name='homepage'),
-    path('menu/', menu_list, name='menu_list'),
-
-    
     path('api/',include('home.urls')),
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
-    path('', include('home.urls')),
 ]
