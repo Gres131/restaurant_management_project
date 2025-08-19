18f933bbfe7eb29b4457694ea342966850064139
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-19 19:35:37 +0530

    34054

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 class RestaurantInfo(models.Model):
@@ -18,3 +19,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    return f"{self.name} ({self.email})"
