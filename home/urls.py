07bb2a2f560b6f78c820aba4302fcd9f45b701da
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-25 19:59:54 +0530

    38085

--- home/urls.py
+++ home/urls.py
@@ -14,7 +14,7 @@ path('menu/', views.menu_list, name='menu_list'),
 path('contact/', views.contact_us, name='contact_us'), 
 path("", views.homepage, name = "homepage"), 
 path('reservations/', views.reservations, name='reservations'),
-path('feedback/', views.feedback, name='feedback'),
+path('feedback/', views.feedback, name='feedback')
 path('api/menu/', menu_view, name='menu_api'),
 path("locations/", views.restaurant_locations, name="restaurant_locations"),
 ]
