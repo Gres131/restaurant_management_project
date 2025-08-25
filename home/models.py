e02e6f8834de5d6d44d82808143a47a0f8bae72c
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-25 18:22:43 +0530

    38085

--- home/models.py
+++ home/models.py
@@ -29,3 +29,15 @@ class Contact(models.Model):
 
 def __str__(self):
     return f"{self.name} ({self.email})"
+
+
+
+class RestaurantLocation(models.Model):
+    address = models.Charfield(max_length=225)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"      
