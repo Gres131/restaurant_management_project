ed113343dd5ab8699325ce1826b5bb0a937ce4bc
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-25 20:19:07 +0530

    38083

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +131,5 @@ RESTAURANT_NAME = "Tasty Bites"
 RESTAURANT_PHONE = "+91-9876543210"
 RESTAURANT_ADDRESS = "123 MG Road, Bengaluru, India"
 RESTAURANT_MAPS_API_KEY
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
