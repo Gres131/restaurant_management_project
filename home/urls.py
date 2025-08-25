51c6af51ef1eb443ee1156e1fd063cabd0d5eb7a
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-25 20:12:07 +0530

    38083

--- home/urls.py
+++ home/urls.py
@@ -10,10 +10,10 @@ handler404 = 'home.views.custom_404_view'
 
 urlpatterns = [ path('', homepage, name='homepage'), 
 path('admin/', admin.site.urls), 
-path('menu/', views.menu_list, name='menu_list'), 
-path('contact/', views.contact_us, name='contact_us'), 
-path("", views.homepage, name = "homepage"), 
-path('reservations/', views.reservations, name='reservations'),
+path("menu/", views.menu_list, name="menu_list"), 
+path("contact/", views.contact_us, name="contact_us"), 
+path(" ", views.homepage, name = "homepage"), 
+path("reservations/", views.reservations, name="reservations"),
 path('feedback/', views.feedback, name='feedback')
 path('api/menu/', menu_view, name='menu_api'),
 path("locations/", views.restaurant_locations, name="restaurant_locations"),
