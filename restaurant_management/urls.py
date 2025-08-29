354c1ac889c257a91c77cc07da0bfb513cd02fc4
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-08-29 13:03:55 +0530

    39547

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
 ]
 
 # serve media files in development
-if settings.DEBUG
-    uripatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
