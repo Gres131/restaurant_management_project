5e9a6e8fbfc7b9ed3b4290b16c642e4801051305
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-09-16 19:47:43 +0530

    44487

--- home/context_processors.py
+++ home/context_processors.py
@@ -4,5 +4,8 @@ from django.utils.timezone import now
 def footer_context(request):
     return {
         "restaurant_name": getattr(settings, "RESTAURANT_NAME, "Default Restaurant"),
-        "current_year": now().year
+        "restaurant_phone": getattr(settings, "RESTAURANT_PHONE", "N/A"),
+        "restaurant_address": getattr(settings, "RESTAURANT_ADDDRESS", ""),
+        "restaurant_email": getattr(settings, "RESTAURANT_EMAIL", ""),
+        "current_year": now().year,
     }
