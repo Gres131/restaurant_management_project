bbc3065c3aa0c24f6028ab91d6e97c299fee89af
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-10-06 19:00:06 +0530

    44958

--- home/urls.py
+++ home/urls.py
@@ -1,24 +1,26 @@
 from django.urls import path
-from .views import *
-from home.views import homepage
-from django.conf.urls import handler404
+from django.contrib import admin
 from . import views
-from .views import feedback_view
-from .views import menu_view
-from .view import MenuCategoryListView
+from .views import(
+    homepage,
+    feedback_view,
+    menu_view,
+    MenuCategoryListView,
+)
 
+# Custom 404 handler
 handler404 = 'home.views.custom_404_view'
 
-urlpatterns = [ path('', homepage, name='homepage'), 
-path('admin/', admin.site.urls), 
-path("menu/", views.menu_list, name="menu_list"), 
-path("contact/", views.contact_us, name="contact_us"), 
-path(" ", views.homepage, name = "homepage"), 
-path("reservations/", views.reservations, name="reservations"),
-path('feedback/', views.feedback, name='feedback')
-path('api/menu/', menu_view, name='menu_api'),
-path("locations/", views.restaurant_locations, name="restaurant_locations"),
-path("about/", views.about,name="about"),
-path("add-to-cart/<int:item_id>/", views.add_to_cart, name="add_to_cart"),
-path('api/categories/', MenuCategoryListView.as_view(), name='menu-category-list'),
+urlpatterns = [ 
+    path('', homepage, name='homepage'), 
+    path('admin/', admin.site.urls), 
+    path("menu/", views.menu_list, name="menu_list"), 
+    path("contact/", views.contact_us, name="contact_us"),  
+    path("reservations/", views.reservations, name="reservations"),
+    path('feedback/', views.feedback, name='feedback')
+    path('api/menu/', menu_view, name='menu_api'),
+    path("locations/", views.restaurant_locations, name="restaurant_locations"),
+    path("about/", views.about,name="about"),
+    path("add-to-cart/<int:item_id>/", views.add_to_cart, name="add_to_cart"),
+    path('api/categories/', MenuCategoryListView.as_view(), name='menu-category-list'),
 ]
