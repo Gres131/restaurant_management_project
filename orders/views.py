5c18ece7aa7482d3743edf85c72bc88a8ec3a542
Gres131 <210142292+Gres131@users.noreply.github.com>
2025-10-15 21:24:03 +0530

    44957

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from orders.utils import generate_coupon_code
+from orders.models import generate_coupon_code
 
-# Create your views here.
+def create_new_coupon():
+    code = generate_coupon_code(12)
+    Coupon.objects.create(code=code, discount_percent=15)
+    print("New coupon created:", code)
